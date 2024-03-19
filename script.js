39b4195b0c440c1aae54477a3a124939395b6f4d
kanhaiya-pms <kanhaiya.psa@postmortemshala.co.in>
2024-03-19 12:47:51 +0530

    finish project

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ var targetrndm;
 
 
 let body = document.getElementById('body')
+let gameover = document.getElementById('gameover')
+let showScore = document.getElementById('showScore')
+let Restart = document.getElementById('Restart')
 let target = document.getElementById('target')
+let networkStatus = document.getElementById('networkStatus')
 
 var score = 0;
+
+
+window.addEventListener('offline', function () {
+    networkStatus.style.display = 'block';
+})
+
+
+
+
+
+
 
 function increaseScore() {
     score += 10;
@@ -34,19 +49,33 @@ function getNewHit() {
 
 
 function runtimer() {
-    let time = 6;
+    let time = 10;
     let clearEvent = setInterval(function () {
         document.getElementById("timer").innerText = time;
         if (time == 0) {
             clearTimeout(clearEvent);
-           body.innerHTML = ''
+            body.style.display = "none";
+            gameover.style.display = "";
+            showScore.innerText = `Your score :  ${score}`;
+            target.innerText = 0;
+            // score = 0;
+            document.getElementById("score").innerHTML =  '0';
+
+            Restart.addEventListener('click', function () {
+                gameover.style.display = "none";
+                body.style.display = "";
+                score = 0;
+                runtimer();
+                makebobble();
+                getNewHit();
+                // increaseScore();
+            })
         }
         time--
     }, 1000)
 
 
 }
-
 
 
 body.addEventListener('click', function (dets) {
@@ -66,6 +95,9 @@ body.addEventListener('click', function (dets) {
 
     }
 })
+
+
+
 
 
 
